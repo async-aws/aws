2e2c63ba1d9d225e4925bc7c50ab0d66fb1acd63
Nyholm <tobias.nyholm@gmail.com>
2020-01-31 09:26:14 +0100

    Added comment

--- src/Aws/Exception/Http/NetworkException.php
+++ src/Aws/Exception/Http/NetworkException.php
@@ -6,6 +6,11 @@ namespace AsyncAws\Aws\Exception\Http;
 
 use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
+/**
+ * Request could not be sent due network error.
+ *
+ * @author Tobias Nyholm <tobias.nyholm@gmail.com>
+ */
 class NetworkException extends \RuntimeException implements HttpException, TransportExceptionInterface
 {
 }
