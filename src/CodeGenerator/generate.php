8b2adbf0f8ad7356a40883bda1c5801cbdeefd98
Jérémy Derussé <jeremy@derusse.com>
2020-02-03 23:50:00 +0100

    Merges commandes together

--- src/CodeGenerator/generate.php
+++ src/CodeGenerator/generate.php
@@ -1,7 +1,6 @@
 <?php
 
-use AsyncAws\CodeGenerator\Command\CreateCommand;
-use AsyncAws\CodeGenerator\Command\RegenerateCommand;
+use AsyncAws\CodeGenerator\Command\GenerateCommand;
 use AsyncAws\CodeGenerator\Generator\ApiGenerator;
 use Symfony\Component\Console\Application;
 
@@ -9,10 +8,8 @@ $application = new Application('Async AWS', '0.1.0');
 
 $src = getenv('ASYNC_AWS_GENERATE_SRC') ?? __DIR__ . '/src';
 $manifest = getenv('ASYNC_AWS_GENERATE_MANIFEST') ?? __DIR__ . '//manifest.json';
-$command = new CreateCommand($manifest, new ApiGenerator($src));
+$command = new GenerateCommand($manifest, new ApiGenerator($src));
 $application->add($command);
-$application->setDefaultCommand($command->getName());
-
-$application->add(new RegenerateCommand($manifest, new ApiGenerator($src)));
+$application->setDefaultCommand($command->getName(), true);
 
 $application->run();
