7c0eb7f18115558eb69de2410d9ddc1694d469fd
Nyholm <tobias.nyholm@gmail.com>
2020-02-06 16:53:59 +0100

    Add missing file

---
+++ src/Sqs/Result/GetQueueUrlResult.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace AsyncAws\Sqs\Result;
+
+use AsyncAws\Core\Result;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Contracts\HttpClient\ResponseInterface;
+
+class GetQueueUrlResult extends Result
+{
+    /**
+     * The URL of the queue.
+     */
+    private $QueueUrl;
+
+    public function getQueueUrl(): ?string
+    {
+        $this->initialize();
+
+        return $this->QueueUrl;
+    }
+
+    protected function populateResult(ResponseInterface $response, ?HttpClientInterface $httpClient): void
+    {
+        $data = new \SimpleXMLElement($response->getContent(false));
+        $data = $data->GetQueueUrlResult;
+
+        $this->QueueUrl = $this->xmlValueOrNull($data->QueueUrl, 'string');
+    }
+}
