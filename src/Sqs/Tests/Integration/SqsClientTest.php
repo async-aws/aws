02099868956f6fa224730ff6083af5b62de544b3
Nyholm <tobias.nyholm@gmail.com>
2020-02-07 22:29:52 +0100

    Added tests for SQS

---
+++ src/Sqs/Tests/Integration/SqsClientTest.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AsyncAws\Sqs\Tests\Integration;
+
+use AsyncAws\Sqs\Input\SendMessageRequest;
+use PHPUnit\Framework\TestCase;
+
+class SqsClientTest extends TestCase
+{
+    use GetClient;
+
+    public function testSendMessage()
+    {
+        $sqs = $this->getClient();
+
+        $result = $sqs->createQueue(['QueueName' => 'bar']);
+        $result->resolve();
+        self::assertEquals(200, $result->info()['status']);
+
+        $input = new SendMessageRequest([]);
+        $input
+            ->setQueueUrl('https://foo.com/bar')
+            ->setMessageBody('foobar');
+
+        $result = $sqs->sendMessage($input);
+        self::assertNotNull($result->getMessageId());
+    }
+}
