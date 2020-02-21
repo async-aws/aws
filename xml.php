12ce11853a54104719d622cc3afe4a21e450b991
Tobias Nyholm <tobias.nyholm@gmail.com>
2020-02-21 23:26:11 +0100

    Delete xml.php

--- xml.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-$document = new \DOMDocument('1.0', 'UTF-8');
-$document->formatOutput = true;
-$action = $document->createElement('ListObjectsV2');
-$document->appendChild($action);
-
-echo $document->saveXML();
