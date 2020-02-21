ed6b6d67c6f208cb2cdef92b54afa31facef8b24
Tobias Nyholm <tobias.nyholm@gmail.com>
2020-02-21 15:09:49 +0100

    Added some more docs to SymfonyBundle (#169)
    * Added some more docs to SymfonyBundle
    
    * Rename async_aws.services to async_aws.clients

--- src/Symfony/Bundle/DependencyInjection/Configuration.php
+++ src/Symfony/Bundle/DependencyInjection/Configuration.php
@@ -17,21 +17,21 @@ class Configuration implements ConfigurationInterface
         /** @var ArrayNodeDefinition $rootNode */
         $rootNode = $treeBuilder->getRootNode();
         $rootNode
-            ->fixXmlConfig('service')
+            ->fixXmlConfig('client')
             ->children()
-                ->booleanNode('register_service')->info('If set to false, no service will be created.')->defaultTrue()->end()
+                ->booleanNode('register_service')->info('If set to false, no services will be created.')->defaultTrue()->end()
                 ->scalarNode('credential_provider')->info('A service name for AsyncAws\Core\Credentials\CredentialProvider.')->defaultNull()->end()
                 ->scalarNode('http_client')->info('A service name for Symfony\Contracts\HttpClient\HttpClientInterface.')->defaultNull()->end()
                 ->scalarNode('logger')->info('A service name for Psr\Log\LoggerInterface.')->end()
-                ->arrayNode('config')->normalizeKeys(false)->prototype('variable')->end()->end()
+                ->arrayNode('config')->info('Default config that will be merged will all services.')->normalizeKeys(false)->prototype('variable')->end()->end()
 
-                ->arrayNode('services')
+                ->arrayNode('clients')
                     ->beforeNormalization()->always()->then(\Closure::fromCallable([$this, 'validateType']))->end()
                     ->useAttributeAsKey('name')
                     ->arrayPrototype()
                         ->children()
-                            ->booleanNode('register_service')->info('If set to false, no service will be created.')->defaultTrue()->end()
-                            ->arrayNode('config')->normalizeKeys(false)->prototype('variable')->end()->end()
+                            ->booleanNode('register_service')->info('If set to false, no service will be created for this AWS type.')->defaultTrue()->end()
+                            ->arrayNode('config')->info('Configuration specific to this service.')->normalizeKeys(false)->prototype('variable')->end()->end()
                             ->enumNode('type')->info('A valid AWS type. The service name will be used as default. ')->values(AwsPackagesProvider::getServiceNames())->end()
                             ->scalarNode('credential_provider')->info('A service name for AsyncAws\Core\Credentials\CredentialProvider.')->end()
                             ->scalarNode('http_client')->info('A service name for Symfony\Contracts\HttpClient\HttpClientInterface.')->end()
@@ -44,24 +44,24 @@ class Configuration implements ConfigurationInterface
         return $treeBuilder;
     }
 
-    private static function validateType(array $services)
+    private static function validateType(array $clients)
     {
         $awsServices = AwsPackagesProvider::getServiceNames();
-        foreach ($services as $name => $config) {
+        foreach ($clients as $name => $config) {
             if (\in_array($name, $awsServices)) {
                 if (isset($config['type']) && $name !== $config['type']) {
                     throw new InvalidConfigurationException(sprintf('You cannot define a service named "%s" with type "%s". That is super confusing.', $name, $config['type']));
                 }
-                $services[$name]['type'] = $name;
+                $clients[$name]['type'] = $name;
             } elseif (!isset($config['type'])) {
                 if (!\in_array($name, $awsServices)) {
-                    throw new InvalidConfigurationException(sprintf('The "async_aws.service.%s" does not have a type. We were unable to guess what AWS service you want. Please add "aws.service.%s.type".', $name, $name));
+                    throw new InvalidConfigurationException(sprintf('The "async_aws.client.%s" does not have a type. We were unable to guess what AWS service you want. Please add "aws.service.%s.type".', $name, $name));
                 }
 
-                $services[$name]['type'] = $name;
+                $clients[$name]['type'] = $name;
             }
         }
 
-        return $services;
+        return $clients;
     }
 }
--- src/Symfony/Bundle/README.md
+++ src/Symfony/Bundle/README.md
@@ -13,12 +13,21 @@ composer require async-aws/async-aws-bundle
 
 ## Configure
 
+The bundle will autowire all AsyncAws clients that you have installed. You can
+provide default configuration to all clients and specific configuration of each
+service you define.
+
 ```yaml
+
+# config/packages/async_aws.yaml
+# This example assume you have installed core, ses, sqs and S3
+#
+
 async_aws:
     config: # Will be merged with other configuration
         region: eu-central-1
 
-    services:
+    clients:
         ses: ~ # This will complain if we dont have that package installed
         sqs:
             config:
@@ -31,13 +40,14 @@ The config above will create the following services:
 
 | Service name               | Autowired with                  |
 | -------------------------- | ------------------------------- |
-| `async_aws.service.ses`    | \AsyncAws\Ses\SesClient
-| `async_aws.service.sqs`    | \AsyncAws\Sqs\SqsClient
-| `async_aws.service.foobar` | \AsyncAws\Sqs\SqsClient $foobar
-| `async_aws.service.s3`     | \AsyncAws\S3\S3Client
+| `async_aws.client.ses`    | \AsyncAws\Ses\SesClient
+| `async_aws.client.sqs`    | \AsyncAws\Sqs\SqsClient
+| `async_aws.client.foobar` | \AsyncAws\Sqs\SqsClient $foobar
+| `async_aws.client.s3`     | \AsyncAws\S3\S3Client
+| `async_aws.client.sts`    | \AsyncAws\Core\Sts\StsClient
 
 For a complete reference of the configuration please run:
 
 ```cli
-php bin/console config:dump-reference asyncaws
+php bin/console config:dump-reference async_aws
 ```
