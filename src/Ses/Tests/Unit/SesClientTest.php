a34ffadbe5d2e7919cb0b44e51190b17ff834906
Nyholm <tobias.nyholm@gmail.com>
2020-02-21 22:41:12 +0100

    Bugfixes

--- src/Ses/Tests/Unit/SesClientTest.php
+++ src/Ses/Tests/Unit/SesClientTest.php
@@ -13,10 +13,7 @@ use AsyncAws\Ses\SesClient;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\HttpClient\MockHttpClient;
 use Symfony\Component\HttpClient\Response\MockResponse;
-use Symfony\Component\Mailer\Bridge\Amazon\Transport\SesApiTransport;
-use Symfony\Component\Mailer\Exception\HttpTransportException;
-use Symfony\Component\Mime\Address;
-use Symfony\Component\Mime\Email;
+
 use Symfony\Contracts\HttpClient\ResponseInterface;
 
 class SesClientTest extends TestCase
@@ -27,20 +24,16 @@ class SesClientTest extends TestCase
             $this->assertSame('POST', $method);
             $this->assertStringStartsWith('https://email.eu-west-1.amazonaws.com:8984/', $url);
 
-            parse_str($options['body'], $content);
+            $content = json_decode($options['body'], true);
 
-            $this->assertSame('Hello!', $content['Content_Simple_Subject_Data']);
-            $this->assertSame('tobias.nyholm@gmail.com', $content['Destination_ToAddresses_1']);
+            $this->assertSame('Hello!', $content['Content']['Simple']['Subject']['Data']);
+            $this->assertSame('tobias.nyholm@gmail.com', $content['Destination']['ToAddresses'][0]);
             $this->assertSame('foo@test.se', $content['FromEmailAddress']);
-            $this->assertSame('Hello There!', $content['Content_Simple_Body_Text_Data']);
+            $this->assertSame('Hello There!', $content['Content']['Simple']['Body']['Text']['Data']);
 
-            $xml = '<SendEmailResponse xmlns="https://email.amazonaws.com/doc/2010-03-31/">
-  <SendEmailResult>
-    <MessageId>foobar</MessageId>
-  </SendEmailResult>
-</SendEmailResponse>';
+            $json = '{"MessageId":"foobar"}';
 
-            return new MockResponse($xml, [
+            return new MockResponse($json, [
                 'http_code' => 200,
             ]);
         });
@@ -70,21 +63,16 @@ class SesClientTest extends TestCase
     public function testSendThrowsForErrorResponse()
     {
         $httpClient = new MockHttpClient(function (string $method, string $url, array $options): ResponseInterface {
-            $xml = "<SendEmailResponse xmlns=\"https://email.amazonaws.com/doc/2010-03-31/\">
-                <Error>
-                    <Message>i'm a teapot</Message>
-                    <Code>418</Code>
-                </Error>
-            </SendEmailResponse>";
+            $json = '{"message":"Missing final \'@domain\'"}';
 
-            return new MockResponse($xml, [
-                'http_code' => 418,
+            return new MockResponse($json, [
+                'http_code' => 400,
             ]);
         });
         $ses = new SesClient([], null, $httpClient);
 
         $input = new SendEmailRequest([
-            'FromEmailAddress' =>'foo@test.se'
+            'FromEmailAddress' =>'foo' // no @test.se
         ]);
         $input->setDestination(new Destination([
             'ToAddresses' => ['tobias.nyholm@gmail.com'],
@@ -98,7 +86,7 @@ class SesClientTest extends TestCase
         $result = $ses->sendEmail($input);
 
         $this->expectException(ClientException::class);
-        $this->expectExceptionMessage('HTTP 418 returned');
+        $this->expectExceptionMessage('HTTP 400 returned');
         $result->resolve();
     }
 }
