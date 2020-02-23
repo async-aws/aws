cbc4d24f79dcb8b18928767b4fc4a99154666a33
Tobias Nyholm <tobias.nyholm@gmail.com>
2020-02-23 12:30:17 +0100

    Added tests for CloudFormation Result (#197)
    * Added tests for CloudFormation Result
    
    * cs

--- src/CloudFormation/Tests/Unit/Result/DescribeStackEventsOutputTest.php
+++ src/CloudFormation/Tests/Unit/Result/DescribeStackEventsOutputTest.php
@@ -3,6 +3,7 @@
 namespace AsyncAws\CloudFormation\Tests\Unit\Result;
 
 use AsyncAws\CloudFormation\Result\DescribeStackEventsOutput;
+use AsyncAws\CloudFormation\Result\StackEvent;
 use AsyncAws\Core\Test\Http\SimpleMockedResponse;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\HttpClient\MockHttpClient;
@@ -11,15 +12,68 @@ class DescribeStackEventsOutputTest extends TestCase
 {
     public function testDescribeStackEventsOutput(): void
     {
-        self::markTestIncomplete('Not implemented');
-
-        $response = new SimpleMockedResponse('<?xml version="1.0" encoding="UTF-8"?>
-            <ChangeIt/>
-        ');
+        $response = new SimpleMockedResponse('<DescribeStackEventsResponse xmlns="http://cloudformation.amazonaws.com/doc/2010-05-15/">
+  <DescribeStackEventsResult>
+    <StackEvents>
+      <member>
+        <EventId>8aa00990-8cde-43d0-8013-20a5e4584626</EventId>
+        <PhysicalResourceId>arn:aws:cloudformation:eu-west-1:32456442:stack/sfhackday-dev/dc183ba1-2120-4eef-8388-970daedb1a85</PhysicalResourceId>
+        <ResourceStatus>UPDATE_COMPLETE</ResourceStatus>
+        <StackId>arn:aws:cloudformation:eu-west-1:32456442:stack/sfhackday-dev/dc183ba1-2120-4eef-8388-970daedb1a85</StackId>
+        <StackName>sfhackday-dev</StackName>
+        <LogicalResourceId>sfhackday-dev</LogicalResourceId>
+        <Timestamp>2019-09-23T21:05:09.542Z</Timestamp>
+        <ResourceType>AWS::CloudFormation::Stack</ResourceType>
+      </member>
+      <member>
+        <EventId>22c2770d-d86a-4366-ae96-21b9dc21606c</EventId>
+        <PhysicalResourceId>arn:aws:cloudformation:eu-west-1:32456442:stack/sfhackday-dev/dc183ba1-2120-4eef-8388-970daedb1a85</PhysicalResourceId>
+        <ResourceStatus>UPDATE_COMPLETE_CLEANUP_IN_PROGRESS</ResourceStatus>
+        <StackId>arn:aws:cloudformation:eu-west-1:32456442:stack/sfhackday-dev/dc183ba1-2120-4eef-8388-970daedb1a85</StackId>
+        <StackName>sfhackday-dev</StackName>
+        <LogicalResourceId>sfhackday-dev</LogicalResourceId>
+        <Timestamp>2019-09-23T21:05:08.979Z</Timestamp>
+        <ResourceType>AWS::CloudFormation::Stack</ResourceType>
+      </member>
+      <member>
+        <EventId>eccbf097-933d-41b7-a3ed-2921340f033b</EventId>
+        <PhysicalResourceId>arn:aws:cloudformation:eu-west-1:32456442:stack/sfhackday-dev/dc183ba1-2120-4eef-8388-970daedb1a85</PhysicalResourceId>
+        <ResourceStatus>CREATE_IN_PROGRESS</ResourceStatus>
+        <ResourceStatusReason>User Initiated</ResourceStatusReason>
+        <StackId>arn:aws:cloudformation:eu-west-1:32456442:stack/sfhackday-dev/dc183ba1-2120-4eef-8388-970daedb1a85</StackId>
+        <StackName>sfhackday-dev</StackName>
+        <LogicalResourceId>sfhackday-dev</LogicalResourceId>
+        <Timestamp>2019-09-23T21:03:09.483Z</Timestamp>
+        <ResourceType>AWS::CloudFormation::Stack</ResourceType>
+      </member>
+    </StackEvents>
+  </DescribeStackEventsResult>
+  <ResponseMetadata>
+    <RequestId>0b836460-dcb3-4e4d-8561-18a3c39bb5aa</RequestId>
+  </ResponseMetadata>
+</DescribeStackEventsResponse>
+');
 
         $result = new DescribeStackEventsOutput($response, new MockHttpClient());
 
-        // self::assertTODO(expected, $result->getStackEvents());
-        self::assertStringContainsString('change it', $result->getNextToken());
+        /** @var StackEvent[] $stackEvents */
+        $stackEvents = [];
+        foreach ($result->getStackEvents(true) as $s) {
+            $stackEvents[] = $s;
+        }
+
+        self::assertCount(3, $stackEvents);
+        self::assertInstanceOf(StackEvent::class, $stackEvents[0]);
+        self::assertEquals('8aa00990-8cde-43d0-8013-20a5e4584626', $stackEvents[0]->getEventId());
+        self::assertEquals('arn:aws:cloudformation:eu-west-1:32456442:stack/sfhackday-dev/dc183ba1-2120-4eef-8388-970daedb1a85', $stackEvents[0]->getPhysicalResourceId());
+        self::assertEquals('UPDATE_COMPLETE', $stackEvents[0]->getResourceStatus());
+        self::assertEquals('arn:aws:cloudformation:eu-west-1:32456442:stack/sfhackday-dev/dc183ba1-2120-4eef-8388-970daedb1a85', $stackEvents[0]->getStackId());
+        self::assertEquals('sfhackday-dev', $stackEvents[0]->getStackName());
+        self::assertEquals('sfhackday-dev', $stackEvents[0]->getLogicalResourceId());
+        self::assertEquals('AWS::CloudFormation::Stack', $stackEvents[0]->getResourceType());
+
+        self::assertNull($stackEvents[0]->getClientRequestToken());
+        self::assertNull($stackEvents[0]->getResourceProperties());
+        self::assertNull($stackEvents[0]->getResourceStatusReason());
     }
 }
--- src/CloudFormation/Tests/Unit/Result/DescribeStacksOutputTest.php
+++ src/CloudFormation/Tests/Unit/Result/DescribeStacksOutputTest.php
@@ -3,23 +3,106 @@
 namespace AsyncAws\CloudFormation\Tests\Unit\Result;
 
 use AsyncAws\CloudFormation\Result\DescribeStacksOutput;
+use AsyncAws\CloudFormation\Result\RollbackConfiguration;
+use AsyncAws\CloudFormation\Result\Stack;
+use AsyncAws\CloudFormation\Result\StackDriftInformation;
+use AsyncAws\CloudFormation\Result\Tag;
 use AsyncAws\Core\Test\Http\SimpleMockedResponse;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\HttpClient\MockHttpClient;
 
 class DescribeStacksOutputTest extends TestCase
 {
-    public function testDescribeStacksOutput(): void
+    public function testDescribeStacksOutput()
     {
-        self::markTestIncomplete('Not implemented');
+        $xml = <<<XML
+<DescribeStacksResponse xmlns="http://cloudformation.amazonaws.com/doc/2010-05-15/">
+  <DescribeStacksResult>
+    <Stacks>
+      <member>
+        <Outputs>
+          <member>
+            <Description>Current Lambda function version</Description>
+            <OutputKey>WebsiteLambdaFunctionQualifiedArn</OutputKey>
+            <OutputValue>arn:aws:lambda:eu-west-1:3425657435:function:sfhackday-dev-website:1</OutputValue>
+          </member>
+          <member>
+            <Description>URL of the service endpoint</Description>
+            <OutputKey>ServiceEndpoint</OutputKey>
+            <OutputValue>https://etysrdhfgd.execute-api.eu-west-1.amazonaws.com/dev</OutputValue>
+          </member>
+          <member>
+            <OutputKey>ServerlessDeploymentBucketName</OutputKey>
+            <OutputValue>sbucket-sdfghggsfsa</OutputValue>
+          </member>
+        </Outputs>
+        <Capabilities>
+          <member>CAPABILITY_IAM</member>
+          <member>CAPABILITY_NAMED_IAM</member>
+        </Capabilities>
+        <CreationTime>2019-09-23T21:03:09.483Z</CreationTime>
+        <NotificationARNs/>
+        <StackId>arn:aws:cloudformation:eu-west-1:45365435:stack/sfhackday-dev/dc183ba1-2120-4eef-8388-970daedb1a85</StackId>
+        <StackName>sfhackday-dev</StackName>
+        <Description>Serverless application</Description>
+        <StackStatus>UPDATE_COMPLETE</StackStatus>
+        <DisableRollback>false</DisableRollback>
+        <Tags>
+          <member>
+            <Value>dev</Value>
+            <Key>STAGE</Key>
+          </member>
+        </Tags>
+        <RollbackConfiguration/>
+        <DriftInformation>
+          <StackDriftStatus>NOT_CHECKED</StackDriftStatus>
+        </DriftInformation>
+        <EnableTerminationProtection>false</EnableTerminationProtection>
+        <LastUpdatedTime>2019-09-23T21:03:50.082Z</LastUpdatedTime>
+      </member>
+    </Stacks>
+  </DescribeStacksResult>
+  <ResponseMetadata>
+    <RequestId>41b5466b-b8c4-45de-9bfb-87dc0d01d85e</RequestId>
+  </ResponseMetadata>
+</DescribeStacksResponse>
 
-        $response = new SimpleMockedResponse('<?xml version="1.0" encoding="UTF-8"?>
-            <ChangeIt/>
-        ');
+XML;
 
+        $response = new SimpleMockedResponse($xml);
         $result = new DescribeStacksOutput($response, new MockHttpClient());
 
-        // self::assertTODO(expected, $result->getStacks());
-        self::assertStringContainsString('change it', $result->getNextToken());
+        $stack = null;
+        foreach ($result->getStacks(true) as $s) {
+            $stack = $s;
+        }
+
+        self::assertInstanceOf(Stack::class, $stack, 'Could not find any stacks');
+        /** @var Stack $stack */
+        self::assertCount(2, $stack->getCapabilities());
+        self::assertEquals(['CAPABILITY_IAM', 'CAPABILITY_NAMED_IAM'], $stack->getCapabilities());
+        self::assertIsArray($stack->getNotificationARNs());
+        self::assertEmpty($stack->getNotificationARNs());
+        self::assertEquals('arn:aws:cloudformation:eu-west-1:45365435:stack/sfhackday-dev/dc183ba1-2120-4eef-8388-970daedb1a85', $stack->getStackId());
+        self::assertEquals('sfhackday-dev', $stack->getStackName());
+        self::assertEquals('Serverless application', $stack->getDescription());
+        self::assertEquals('UPDATE_COMPLETE', $stack->getStackStatus());
+        self::assertEquals(false, $stack->getDisableRollback());
+        self::assertCount(1, $stack->getTags());
+        $tag = $stack->getTags()[0];
+        self::assertInstanceOf(Tag::class, $tag);
+        self::assertEquals('dev', $tag->getValue());
+        self::assertEquals('STAGE', $tag->getKey());
+        self::assertInstanceOf(RollbackConfiguration::class, $stack->getRollbackConfiguration());
+        self::assertNull($stack->getRollbackConfiguration()->getMonitoringTimeInMinutes());
+        self::assertIsArray($stack->getRollbackConfiguration()->getRollbackTriggers());
+        self::assertEmpty($stack->getRollbackConfiguration()->getRollbackTriggers());
+        self::assertInstanceOf(StackDriftInformation::class, $stack->getDriftInformation());
+        self::assertEquals('NOT_CHECKED', $stack->getDriftInformation()->getStackDriftStatus());
+        self::assertFalse($stack->getEnableTerminationProtection());
+
+        $output = $stack->getOutputs();
+        self::assertCount(3, $output);
+        self::assertEquals('WebsiteLambdaFunctionQualifiedArn', $output[0]->getOutputKey());
     }
 }
