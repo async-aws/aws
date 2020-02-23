20743d5d2a5d46d2ade2561047dd89eae96f3db5
Nyholm <tobias.nyholm@gmail.com>
2020-02-23 18:01:40 +0100

    Rename AwsClient to AwsClientFactory

--- tests/Unit/Core/AwsClientFactoryTest.php
+++ tests/Unit/Core/AwsClientFactoryTest.php
@@ -4,24 +4,24 @@ declare(strict_types=1);
 
 namespace AsyncAws\Test\Unit\Core;
 
-use AsyncAws\Core\AwsClient;
+use AsyncAws\Core\AwsClientFactory;
 use AsyncAws\Test\ServiceProvider;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\HttpClient\MockHttpClient;
 
-class AwsClientTest extends TestCase
+class AwsClientFactoryTest extends TestCase
 {
     public function testMethodExists()
     {
-        $awsClient = new AwsClient([], null, new MockHttpClient());
+        $awsClient = new AwsClientFactory([], null, new MockHttpClient());
         foreach (ServiceProvider::getAwsServices() as $serviceName => $serviceData) {
             $method = lcfirst($serviceName);
-            self::assertTrue(method_exists(AwsClient::class, $method), sprintf('The "%s" should have a function named "%s"', AwsClient::class, $method));
+            self::assertTrue(method_exists(AwsClientFactory::class, $method), sprintf('The "%s" should have a function named "%s"', AwsClientFactory::class, $method));
 
             $result = $awsClient->$method();
             $namespace = $serviceData['namespace'] ?? sprintf('AsyncAws\\%s', $serviceName);
             $fqcn = sprintf('%s\\%sClient', $namespace, $serviceName);
-            self::assertInstanceOf($fqcn, $result, sprintf('Calling "%s::%s" should return a "%s". Instance of "%s" given.', AwsClient::class, $method, $fqcn, \get_class($result)));
+            self::assertInstanceOf($fqcn, $result, sprintf('Calling "%s::%s" should return a "%s". Instance of "%s" given.', AwsClientFactory::class, $method, $fqcn, \get_class($result)));
         }
     }
 }
