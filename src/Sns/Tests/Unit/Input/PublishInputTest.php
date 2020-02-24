56443497de757f847a37bbd796d1d4d02626d645
Nyholm <tobias.nyholm@gmail.com>
2020-02-24 19:17:40 +0100

    Use attributes with SNS

--- src/Sns/Tests/Unit/Input/PublishInputTest.php
+++ src/Sns/Tests/Unit/Input/PublishInputTest.php
@@ -21,6 +21,11 @@ class PublishInputTest extends TestCase
         $input = new PublishInput([
             'TopicArn' => 'arn:aws:sns:us-east-1:46563727:async',
             'Message' => 'Foo',
+            'Subject' => 'MySubject',
+            'MessageAttributes' => ['myAttribute' => new MessageAttributeValue([
+                'DataType' => 'String',
+                'StringValue' => 'Foobar',
+            ])],
         ]);
 
         $expected = '
@@ -28,6 +33,10 @@ class PublishInputTest extends TestCase
             &Version=2010-03-31
             &TopicArn=arn%3Aaws%3Asns%3Aus-east-1%3A46563727%3Aasync
             &Message=Foo
+            &Subject=MySubject
+            &MessageAttributes.entry.1.Name=myAttribute
+            &MessageAttributes.entry.1.Value.DataType=String
+            &MessageAttributes.entry.1.Value.StringValue=Foobar
         ';
 
         self::assertHttpFormEqualsHttpForm($expected, $input->requestBody());
