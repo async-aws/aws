cd58a2be91d39c4fbc081598fdefb038f6b50a35
Nyholm <tobias.nyholm@gmail.com>
2020-02-27 11:48:21 +0100

    Use better autoloading for test files

---
+++ src/Core/Test/PHPUnitTestCase.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace AsyncAws\Core\Test;
+
+use PHPUnit\Framework\TestCase;
+
+/*
+ *  Because AsyncAws use symfony/phpunit-bridge and don't requires phpunit/phpunit,
+ *  this class may not exits but is required by the generator and static analyzer tools
+ */
+if (!\class_exists(TestCase::class)) {
+    class PHPUnitTestCase
+    {
+        public static function assertEqualsCanonicalizing($expected, $actual, string $message = ''): void
+        {
+        }
+    }
+
+    // If the PHPUnit is not installed, use this class instead.
+    \class_alias(PHPUnitTestCase::class, TestCase::class);
+}
