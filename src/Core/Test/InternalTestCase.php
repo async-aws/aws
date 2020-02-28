dace0836ac8d6aaf700193fca62eda213a96da4f
Tobias Nyholm <tobias.nyholm@gmail.com>
2020-02-28 05:41:27 +0100

    Use better autoloading for test files (#229)
    * Use better autoloading for test files
    
    * CS
    
    * Dont use alias
    
    * improvements
    
    * cs
    
    * Update branch alias
    
    * [S3] Update core version

---
+++ src/Core/Test/InternalTestCase.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AsyncAws\Core\Test;
+
+/**
+ * Because AsyncAws use symfony/phpunit-bridge and don't requires phpunit/phpunit,
+ * this class may not exits but is required by the generator and static analyzer tools.
+ *
+ * @internal use AsyncAws\Core\Test\TestCase instead
+ */
+class InternalTestCase
+{
+    public static function assertEqualsCanonicalizing($expected, $actual, string $message = ''): void
+    {
+    }
+}
