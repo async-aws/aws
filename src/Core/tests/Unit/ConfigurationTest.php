009f3467cd26bda30baf8150cb6dea8fe5d47e00
Nyholm <tobias.nyholm@gmail.com>
2020-03-06 13:50:43 +0100

    cs

--- src/Core/tests/Unit/ConfigurationTest.php
+++ src/Core/tests/Unit/ConfigurationTest.php
@@ -12,27 +12,27 @@ class ConfigurationTest extends TestCase
     public function testDefaultValues()
     {
         $config = Configuration::create(['endpoint' => 'foo']);
-        $this->assertEquals('foo', $config->get('endpoint'));
+        self::assertEquals('foo', $config->get('endpoint'));
 
         $config = Configuration::create(['endpoint' => '']);
-        $this->assertEquals('', $config->get('endpoint'));
+        self::assertEquals('', $config->get('endpoint'));
 
         $config = Configuration::create([]);
-        $this->assertEquals('https://%service%.%region%.amazonaws.com', $config->get('endpoint'));
+        self::assertEquals('https://%service%.%region%.amazonaws.com', $config->get('endpoint'));
 
         // If a configuration value is set to null, we should use default value.
         $config = Configuration::create(['endpoint' => null]);
-        $this->assertEquals('https://%service%.%region%.amazonaws.com', $config->get('endpoint'));
+        self::assertEquals('https://%service%.%region%.amazonaws.com', $config->get('endpoint'));
 
         $config = Configuration::create([]);
-        $this->assertEquals('default', $config->get('profile'));
+        self::assertEquals('default', $config->get('profile'));
 
         putenv('AWS_PROFILE=foo');
         $config = Configuration::create([]);
-        $this->assertEquals('foo', $config->get('profile'));
+        self::assertEquals('foo', $config->get('profile'));
 
         putenv('AWS_PROFILE');
         $config = Configuration::create([]);
-        $this->assertEquals('default', $config->get('profile'));
+        self::assertEquals('default', $config->get('profile'));
     }
 }
