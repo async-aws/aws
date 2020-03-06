4319aa38e3cf25a3ff7dda8f6d31a334e8ca8d33
Nyholm <tobias.nyholm@gmail.com>
2020-03-06 13:48:54 +0100

    Added more tests

--- src/Core/tests/Unit/ConfigurationTest.php
+++ src/Core/tests/Unit/ConfigurationTest.php
@@ -9,17 +9,30 @@ use PHPUnit\Framework\TestCase;
 
 class ConfigurationTest extends TestCase
 {
-    /**
-     * If a configuration value is set to null, we should use default value.
-     */
     public function testDefaultValues()
     {
         $config = Configuration::create(['endpoint' => 'foo']);
         $this->assertEquals('foo', $config->get('endpoint'));
+
         $config = Configuration::create(['endpoint' => '']);
         $this->assertEquals('', $config->get('endpoint'));
 
+        $config = Configuration::create([]);
+        $this->assertEquals('https://%service%.%region%.amazonaws.com', $config->get('endpoint'));
+
+        // If a configuration value is set to null, we should use default value.
         $config = Configuration::create(['endpoint' => null]);
         $this->assertEquals('https://%service%.%region%.amazonaws.com', $config->get('endpoint'));
+
+        $config = Configuration::create([]);
+        $this->assertEquals('default', $config->get('profile'));
+
+        putenv('AWS_PROFILE=foo');
+        $config = Configuration::create([]);
+        $this->assertEquals('foo', $config->get('profile'));
+
+        putenv('AWS_PROFILE');
+        $config = Configuration::create([]);
+        $this->assertEquals('default', $config->get('profile'));
     }
 }
