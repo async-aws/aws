ce9e730970fc750c7559b69da89703702f6a74c4
Nyholm <tobias.nyholm@gmail.com>
2020-03-14 20:28:17 +0100

    More debug

--- src/Integration/Flysystem/S3/tests/bootstrap.php
+++ src/Integration/Flysystem/S3/tests/bootstrap.php
@@ -2,12 +2,13 @@
 
 declare(strict_types=1);
 
-die('flysustem boostrap file');
 require_once \dirname(__DIR__) . '/vendor/autoload.php';
 $file = \dirname(__DIR__) . '/vendor/league/flysystem/test-functions.php';
 if (file_exists($file)) {
+    die('File does exist: '.$file);
     require_once $file;
 }
+die('Can NOT find file: '.$file);
 
 require_once __DIR__ . '/version-bridge.php';
 require_once __DIR__ . '/version-bridge-tests.php';
