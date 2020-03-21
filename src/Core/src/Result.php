761f0f3e8064f57ff044124b38d44a5f92bfa2a0
Nyholm <tobias.nyholm@gmail.com>
2020-03-21 21:49:25 +0100

    Updated comment

--- src/Core/src/Result.php
+++ src/Core/src/Result.php
@@ -8,8 +8,7 @@ use AsyncAws\Core\Exception\Http\HttpException;
 use AsyncAws\Core\Exception\Http\NetworkException;
 
 /**
- * The result promise is always returned from every API call. Remember to call `resolve()` to
- * make sure the request is actually sent.
+ * Base class for all return values from a service's operation. Eg: FooClient::bar(): Result
  */
 class Result
 {
