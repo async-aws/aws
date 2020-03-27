281792c860d8f2222359c20e4add2364dadcbcaa
Nyholm <tobias.nyholm@gmail.com>
2020-03-27 09:02:47 +0100

    Removed comments

--- website/webpack.config.js
+++ website/webpack.config.js
@@ -19,9 +19,6 @@ Encore
     .copyFiles({
         from: './assets/image',
 
-        // optional target path, relative to the output dir
-        //to: 'images/[path][name].[ext]',
-
         // if versioning is enabled, add the file hash too
         to: 'image/[path][name].[ext]',
         //to: 'image/[path][name].[hash:8].[ext]',
@@ -53,10 +50,6 @@ Encore
 
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
-
-// uncomment if you use API Platform Admin (composer require api-admin)
-//.enableReactPreset()
-//.addEntry('admin', './assets/js/admin.js')
 ;
 
 module.exports = Encore.getWebpackConfig();
