aaf49e9e861c1c6cb0ba1bc459b86560101c4375
Tobias Nyholm <tobias.nyholm@gmail.com>
2020-04-11 11:04:00 +0200

    [Flysystem] Remove s3:0.3 code (#473)
    * Remove s3:0.3 code
    
    * Remove "dev"
    
    * Added CHANGELOG.md

--- src/Integration/Flysystem/S3/src/S3FilesystemV2.php
+++ src/Integration/Flysystem/S3/src/S3FilesystemV2.php
@@ -8,9 +8,6 @@ use AsyncAws\Core\Exception\Http\ClientException;
 use AsyncAws\Core\Stream\ResultStream;
 use AsyncAws\Flysystem\S3\Visibility\PortableVisibilityConverter;
 use AsyncAws\Flysystem\S3\Visibility\VisibilityConverter;
-use AsyncAws\S3\Input\ObjectIdentifier as InputObjectIdentifier;
-use AsyncAws\S3\Result\AwsObject as ResultAwsObject;
-use AsyncAws\S3\Result\CommonPrefix as ResultCommonPrefix;
 use AsyncAws\S3\Result\HeadObjectOutput;
 use AsyncAws\S3\S3Client;
 use AsyncAws\S3\ValueObject\AwsObject;
@@ -431,14 +428,4 @@ class S3FilesystemV2 implements FilesystemAdapter
             throw UnableToReadFile::fromLocation($path, '', $exception);
         }
     }
-}
-
-// to remove wth release of 0.4 of async-aws/s3
-if (!\class_exists(AwsObject::class)) {
-    /** @psalm-suppress UndefinedClass */
-    \class_alias(ResultAwsObject::class, AwsObject::class);
-    /** @psalm-suppress UndefinedClass */
-    \class_alias(ResultCommonPrefix::class, CommonPrefix::class);
-    /** @psalm-suppress UndefinedClass */
-    \class_alias(InputObjectIdentifier::class, ObjectIdentifier::class);
 }
