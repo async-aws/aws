7e7d5fb4073ccb1fae7248aca0e18f7833cacfc9
Jérémy Derussé <jeremy@derusse.com>
2020-04-15 21:36:49 +0200

    Fix CS

--- src/Integration/Flysystem/S3/tests/version-bridge.php
+++ src/Integration/Flysystem/S3/tests/version-bridge.php
@@ -6,7 +6,7 @@ declare(strict_types=1);
  * Create necessary classes to be compatible with flysystem v1 and v2.
  */
 
-namespace League\Flysystem {
+namespace League\Flysystem;
 
     // Visibility exists in v2
     if (!class_exists(Visibility::class)) {
@@ -16,4 +16,3 @@ namespace League\Flysystem {
             public const PRIVATE = 'private';
         }
     }
-}
