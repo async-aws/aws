03991d08455b8fb2c3e4adb6f5d7950825737fea
Nyholm <tobias.nyholm@gmail.com>
2020-05-07 00:57:55 +0200

    Adding Illuminate Filesystem

---
+++ src/Integration/Laravel/Filesystem/src/AsyncAwsFilesystemManager.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace AsyncAws\Illuminate\Filesystem;
+
+use AsyncAws\Flysystem\S3\S3FilesystemV1;
+use AsyncAws\S3\S3Client;
+use Illuminate\Contracts\Filesystem\Cloud;
+use Illuminate\Filesystem\FilesystemAdapter;
+use Illuminate\Filesystem\FilesystemManager;
+
+class AsyncAwsFilesystemManager extends FilesystemManager
+{
+    /**
+     * Create an instance of the Amazon S3 driver.
+     *
+     * @return Cloud
+     */
+    public function createAsyncAwsS3Driver(array $config)
+    {
+        $s3Config = [];
+        if ($config['key'] && $config['secret']) {
+            $s3Config['accessKeyId'] = $config['key'] ?? null;
+            $s3Config['accessKeySecret'] = $config['secret'] ?? null;
+            $s3Config['sessionToken'] = $config['token'] ?? null;
+        }
+
+        $root = $config['root'] ?? '';
+        $options = $config['options'] ?? [];
+
+        $s3Client = new S3Client($s3Config);
+        if (class_exists(AbstractAdapter::class)) {
+            $flysystemAdapter = new S3FilesystemV1($s3Client, $config['bucket'], $root, $options);
+        } else {
+            throw new \RuntimeException('Could not use AsyncAwsS3 since Flyststem v1 is not installed.');
+        }
+
+        return new FilesystemAdapter($this->createFlysystem($flysystemAdapter, $config));
+    }
+}
