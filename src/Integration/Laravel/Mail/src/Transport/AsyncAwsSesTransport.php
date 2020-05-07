f0aacf854814bb8ecb85080c961e5935004417d0
Tobias Nyholm <tobias.nyholm@gmail.com>
2020-05-07 15:21:51 +0200

    Added first version of Illuminate Mail (#550)
    * Added first version of Illuminate Mail
    
    * Fixed namespace
    
    * Updated Mail client
    
    * CS fixes
    
    * minor
    
    * Use message
    
    * Handle $form as array and strings
    
    * CS
    
    * Use RawMessage
    
    * Ignore PHPStan error
    
    * cs

---
+++ src/Integration/Laravel/Mail/src/Transport/AsyncAwsSesTransport.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace AsyncAws\Illuminate\Mail\Transport;
+
+use AsyncAws\Ses\SesClient;
+use AsyncAws\Ses\ValueObject\EmailContent;
+use AsyncAws\Ses\ValueObject\RawMessage;
+use Illuminate\Mail\Transport\Transport;
+use Swift_Mime_SimpleMessage;
+
+/**
+ * This class is a port from Illuminate\Mail\Transport\SesTransport.
+ */
+class AsyncAwsSesTransport extends Transport
+{
+    /**
+     * The Amazon SES instance.
+     *
+     * @var SesClient
+     */
+    protected $ses;
+
+    /**
+     * The Amazon SES transmission options.
+     *
+     * @var array
+     */
+    protected $options = [];
+
+    /**
+     * Create a new SES transport instance.
+     *
+     * @param array $options
+     *
+     * @return void
+     */
+    public function __construct(SesClient $ses, $options = [])
+    {
+        $this->ses = $ses;
+        $this->options = $options;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function send(Swift_Mime_SimpleMessage $message, &$failedRecipients = null)
+    {
+        $this->beforeSendPerformed($message);
+
+        $input = [
+            'Content' => new EmailContent([
+                'Raw' => new RawMessage(['Data' => $message->toString()]),
+            ]),
+        ];
+
+        $from = $message->getSender() ?: $message->getFrom();
+        if (\is_array($from)) {
+            $from = reset($from);
+        }
+        if (\is_string($from)) {
+            $input['FromEmailAddress'] = $from;
+        }
+
+        $result = $this->ses->sendEmail(array_merge($this->options, $input));
+
+        $message->getHeaders()->addTextHeader('X-SES-Message-ID', $result->getMessageId());
+        $this->sendPerformed($message);
+
+        return $this->numberOfRecipients($message);
+    }
+
+    /**
+     * Get the Amazon SES client for the SesTransport instance.
+     *
+     * @return SesClient
+     */
+    public function ses()
+    {
+        return $this->ses;
+    }
+
+    /**
+     * Get the transmission options being used by the transport.
+     *
+     * @return array
+     */
+    public function getOptions()
+    {
+        return $this->options;
+    }
+
+    /**
+     * Set the transmission options being used by the transport.
+     *
+     * @return array
+     */
+    public function setOptions(array $options)
+    {
+        return $this->options = $options;
+    }
+}
