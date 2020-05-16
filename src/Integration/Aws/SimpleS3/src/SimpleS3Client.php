808d655ff65845849ef613ef6eb7033fe46c28df
Nyholm <tobias.nyholm@gmail.com>
2020-05-16 09:20:07 +0200

    Init SimpleS3.

---
+++ src/Integration/Aws/SimpleS3/src/SimpleS3Client.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AsyncAws\SimpleS3;
+
+use AsyncAws\S3\Input\GetObjectRequest;
+use AsyncAws\S3\S3Client;
+
+/**
+ * A simplified S3 client that hides some of the complexity of working with S3.
+ * The aim of this client is to provide shortcut methods to about 80% common tasks.
+ *
+ * @author Tobias Nyholm <tobias.nyholm@gmail.com>
+ */
+class SimpleS3Client extends S3Client
+{
+    public function getUrl(string $bucket, string $key): string
+    {
+        $input = new GetObjectRequest([
+            'Bucket' => $bucket,
+            'Key' => $key,
+        ]);
+
+        $url = $this->presign($input, new \DateTimeImmutable('+10minutes'));
+
+        // remove all query parameters.
+        if (false === $pos = strpos($url, '?')) {
+            throw new \RuntimeException('Expected presigned URL to include a query string');
+        }
+
+        return substr($url, 0, $pos);
+    }
+
+    /*
+     * TODO Add get() and put() which supports large files
+     * TODO add has()
+     */
+}
