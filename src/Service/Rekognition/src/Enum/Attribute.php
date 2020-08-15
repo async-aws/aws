f01fc12052d429e74427dfc9d1b086fdde718c28
manuel <pompello98@gmail.com>
2020-08-14 19:51:57 -0500

    Attribute Enum not created by code generator.

---
+++ src/Service/Rekognition/src/Enum/Attribute.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace AsyncAws\Rekognition\Enum;
+
+
+final class Attribute
+{
+    public const DEFAULT = 'DEFAULT';
+    public const ALL = 'ALL';
+
+    public static function exists(string $value): bool
+    {
+        return isset([
+                self::DEFAULT => true,
+                self::ALL => true,
+            ][$value]);
+    }
+}
