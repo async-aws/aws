b1bdf4f98db4c20fcc31ac2264e17b621d80f7f8
Jérémy Derussé <jeremy@derusse.com>
2020-10-19 10:47:34 +0200

    Retry non-idempotent methods

--- src/Core/src/HttpClient/AwsRetryStrategy.php
+++ src/Core/src/HttpClient/AwsRetryStrategy.php
@@ -13,6 +13,14 @@ use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
  */
 class AwsRetryStrategy extends GenericRetryStrategy
 {
+    public const DEFAULT_RETRY_STATUS_CODES = [0, 423, 425, 429, 500, 502, 503, 504, 507, 510];
+
+    // Override Symfony default options for a better integration of AWS servers.
+    public function __construct(array $statusCodes = self::DEFAULT_RETRY_STATUS_CODES, int $delayMs = 1000, float $multiplier = 2.0, int $maxDelayMs = 0, float $jitter = 0.1)
+    {
+        parent::__construct($statusCodes, $delayMs, $multiplier, $maxDelayMs, $jitter);
+    }
+
     public function shouldRetry(AsyncContext $context, ?string $responseContent, ?TransportExceptionInterface $exception): ?bool
     {
         if (parent::shouldRetry($context, $responseContent, $exception)) {
