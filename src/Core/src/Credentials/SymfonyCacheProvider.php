88e2d2e09378074b8178c61fc2ea65c090e45493
Nyholm <tobias.nyholm@gmail.com>
2020-12-04 18:04:53 +0100

    Ignore cache errors

--- src/Core/src/Credentials/SymfonyCacheProvider.php
+++ src/Core/src/Credentials/SymfonyCacheProvider.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace AsyncAws\Core\Credentials;
 
 use AsyncAws\Core\Configuration;
+use Psr\Cache\CacheException;
+use Psr\Log\LoggerInterface;
 use Symfony\Contracts\Cache\CacheInterface;
 use Symfony\Contracts\Cache\ItemInterface;
 
@@ -23,16 +25,20 @@ final class SymfonyCacheProvider implements CredentialProvider
 
     private $decorated;
 
-    public function __construct(CredentialProvider $decorated, CacheInterface $cache)
+    private $logger;
+
+    public function __construct(CredentialProvider $decorated, CacheInterface $cache, ?LoggerInterface $logger = null)
     {
         $this->decorated = $decorated;
         $this->cache = $cache;
+        $this->logger = $logger;
     }
 
     public function getCredentials(Configuration $configuration): ?Credentials
     {
-        return $this->cache->get('AsyncAws.Credentials.' . sha1(\serialize([$configuration, \get_class($this->decorated)])), function (ItemInterface $item) use ($configuration) {
-            $credential = $this->decorated->getCredentials($configuration);
+        $provider = $this->decorated;
+        $closure = \Closure::fromCallable(static function (ItemInterface $item) use ($configuration, $provider) {
+            $credential = $provider->getCredentials($configuration);
 
             if (null !== $credential && null !== $exp = $credential->getExpireDate()) {
                 $item->expiresAt($exp);
@@ -42,5 +48,15 @@ final class SymfonyCacheProvider implements CredentialProvider
 
             return $credential;
         });
+
+        try {
+            return $this->cache->get('AsyncAws.Credentials.' . sha1(\serialize([$configuration, \get_class($this->decorated)])), $closure);
+        } catch (CacheException $e) {
+            if (null !== $this->logger) {
+                $this->logger->error('Failed to get AWS credentials from cache.', ['exception' => $e]);
+            }
+
+            return $provider->getCredentials($configuration);
+        }
     }
 }
