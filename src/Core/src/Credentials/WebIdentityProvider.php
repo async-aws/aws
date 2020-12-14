e7535bced59dcad5cf5b07589e8f6fbe81a510e3
Alessandro Lai <alessandro.lai85@gmail.com>
2020-12-14 11:54:06 +0100

    Clear realpath cache when reading the token from file (#901)
    * Apply same fix as aws/aws-sdk-php#2043
    
    * Fix CS
    
    * Add intermediate variables to save on duplicate function calls
    
    * Update src/Core/src/Credentials/WebIdentityProvider.php
    
    Co-authored-by: Tobias Nyholm <tobias.nyholm@gmail.com>

--- src/Core/src/Credentials/WebIdentityProvider.php
+++ src/Core/src/Credentials/WebIdentityProvider.php
@@ -89,9 +89,7 @@ final class WebIdentityProvider implements CredentialProvider
         }
 
         try {
-            if (false === $token = file_get_contents($tokenFile)) {
-                throw new RuntimeException('failed to read data');
-            }
+            $token = $this->getTokenFileContent($tokenFile);
         } catch (\Exception $e) {
             $this->logger->warning('"Error reading WebIdentityTokenFile "{tokenFile}.', ['tokenFile' => $tokenFile, 'exception' => $e]);
 
@@ -121,5 +119,27 @@ final class WebIdentityProvider implements CredentialProvider
             $credentials->getSessionToken(),
             Credentials::adjustExpireDate($credentials->getExpiration(), $this->getDateFromResult($result))
         );
+    }
+
+    /**
+     * @see https://github.com/async-aws/aws/issues/900
+     * @see https://github.com/aws/aws-sdk-php/issues/2014
+     * @see https://github.com/aws/aws-sdk-php/pull/2043
+     */
+    private function getTokenFileContent(string $tokenFile): string
+    {
+        if (!file_exists($tokenFile)) {
+            $tokenDir = \dirname($tokenFile);
+            $tokenLink = readlink($tokenFile);
+            clearstatcache(true, $tokenDir . \DIRECTORY_SEPARATOR . $tokenLink);
+            clearstatcache(true, $tokenDir . \DIRECTORY_SEPARATOR . \dirname($tokenLink));
+            clearstatcache(true, $tokenFile);
+        }
+
+        if (false === $token = file_get_contents($tokenFile)) {
+            throw new RuntimeException('Failed to read data');
+        }
+
+        return $token;
     }
 }
