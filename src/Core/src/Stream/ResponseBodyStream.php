2e94d13985e4cbd0fce44e6a0a0a6aa7cfc34954
Jérémy Derussé <jeremy@derusse.com>
2021-04-24 18:31:18 +0200

    Bump CI tools

--- src/Core/src/Stream/ResponseBodyStream.php
+++ src/Core/src/Stream/ResponseBodyStream.php
@@ -77,6 +77,7 @@ class ResponseBodyStream implements ResultStream
             }
         }
 
+        /** @psalm-suppress PossiblyNullReference */
         return $this->fallback->getContentAsString();
     }
 
@@ -91,6 +92,7 @@ class ResponseBodyStream implements ResultStream
             }
         }
 
+        /** @psalm-suppress PossiblyNullReference */
         return $this->fallback->getContentAsResource();
     }
 }
