4816f839e417a366549560b1d64f5a3c7de94a97
Jérémy Derussé <jeremy@derusse.com>
2021-09-30 12:56:02 +0200

    Fix psalm false report

--- src/Service/Lambda/src/Enum/Architecture.php
+++ src/Service/Lambda/src/Enum/Architecture.php
@@ -4,14 +4,14 @@ namespace AsyncAws\Lambda\Enum;
 
 final class Architecture
 {
-    public const ARM_64 = 'arm64';
-    public const X_86_64 = 'x86_64';
+    public const ARM64 = 'arm64';
+    public const X86_64 = 'x86_64';
 
     public static function exists(string $value): bool
     {
         return isset([
-            self::ARM_64 => true,
-            self::X_86_64 => true,
+            self::ARM64 => true,
+            self::X86_64 => true,
         ][$value]);
     }
 }
