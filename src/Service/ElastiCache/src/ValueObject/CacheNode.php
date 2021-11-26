e94a2358b9bed6bbce36b7c37d7c24fbf1570012
AsyncAws <61784373+async-aws-bot@users.noreply.github.com>
2021-11-25 23:05:48 -0800

    update generated code (#1135)

--- src/Service/ElastiCache/src/ValueObject/CacheNode.php
+++ src/Service/ElastiCache/src/ValueObject/CacheNode.php
@@ -11,9 +11,8 @@ namespace AsyncAws\ElastiCache\ValueObject;
  * - General purpose:
  *
  *   - Current generation:
- *     **M6g node types** (available only for Redis engine version 5.0.6 onward and for Memcached engine version 1.5.16
- *     onward).
- *     `cache.m6g.large`, `cache.m6g.xlarge`, `cache.m6g.2xlarge`, `cache.m6g.4xlarge`, `cache.m6g.8xlarge`,
+ *     **M6g node types:** (available only for Redis engine version 5.0.6 onward and for Memcached engine version 1.5.16
+ *     onward): `cache.m6g.large`, `cache.m6g.xlarge`, `cache.m6g.2xlarge`, `cache.m6g.4xlarge`, `cache.m6g.8xlarge`,
  *     `cache.m6g.12xlarge`, `cache.m6g.16xlarge`
  *
  *     > For region availability, see Supported Node Types
@@ -22,6 +21,8 @@ namespace AsyncAws\ElastiCache\ValueObject;
  *     `cache.m5.12xlarge`, `cache.m5.24xlarge`
  *     **M4 node types:**`cache.m4.large`, `cache.m4.xlarge`, `cache.m4.2xlarge`, `cache.m4.4xlarge`,
  *     `cache.m4.10xlarge`
+ *     **T4g node types** (available only for Redis engine version 5.0.6 onward and for Memcached engine version 1.5.16
+ *     onward): `cache.t4g.micro`, `cache.t4g.small`, `cache.t4g.medium`
  *     **T3 node types:**`cache.t3.micro`, `cache.t3.small`, `cache.t3.medium`
  *     **T2 node types:**`cache.t2.micro`, `cache.t2.small`, `cache.t2.medium`
  *   - Previous generation: (not recommended)
@@ -34,12 +35,16 @@ namespace AsyncAws\ElastiCache\ValueObject;
  *   - Previous generation: (not recommended)
  *     **C1 node types:**`cache.c1.xlarge`
  *
- * - Memory optimized:
+ * - Memory optimized with data tiering:
  *
  *   - Current generation:
  *     **R6gd node types** (available only for Redis engine version 6.2 onward).
  *     `cache.r6gd.xlarge`, `cache.r6gd.2xlarge`, `cache.r6gd.4xlarge`, `cache.r6gd.8xlarge`, `cache.r6gd.12xlarge`,
  *     `cache.r6gd.16xlarge`
+ *
+ * - Memory optimized:
+ *
+ *   - Current generation:
  *     **R6g node types** (available only for Redis engine version 5.0.6 onward and for Memcached engine version 1.5.16
  *     onward).
  *     `cache.r6g.large`, `cache.r6g.xlarge`, `cache.r6g.2xlarge`, `cache.r6g.4xlarge`, `cache.r6g.8xlarge`,
