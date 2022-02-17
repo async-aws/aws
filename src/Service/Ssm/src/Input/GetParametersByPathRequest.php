836691e69adc1bf42edfdc7b3f116c69e7be6e22
AsyncAws <61784373+async-aws-bot@users.noreply.github.com>
2022-02-16 23:31:27 -0800

    update generated code (#1185)

--- src/Service/Ssm/src/Input/GetParametersByPathRequest.php
+++ src/Service/Ssm/src/Input/GetParametersByPathRequest.php
@@ -11,8 +11,8 @@ use AsyncAws\Ssm\ValueObject\ParameterStringFilter;
 final class GetParametersByPathRequest extends Input
 {
     /**
-     * The hierarchy for the parameter. Hierarchies start with a forward slash (/). The hierachy is the parameter name
-     * except the last part of the parameter. For the API call to succeeed, the last part of the parameter name can't be in
+     * The hierarchy for the parameter. Hierarchies start with a forward slash (/). The hierarchy is the parameter name
+     * except the last part of the parameter. For the API call to succeed, the last part of the parameter name can't be in
      * the path. A parameter name hierarchy can have a maximum of 15 levels. Here is an example of a hierarchy:
      * `/Finance/Prod/IAD/WinServ2016/license33 `.
      *
