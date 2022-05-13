7b3f959d58fa5214596a1a95614b25cb127171a9
AsyncAws <61784373+async-aws-bot@users.noreply.github.com>
2022-05-13 00:56:35 -0700

    Update generated code (#1237)
    * update generated code
    
    * Update src/Service/Lambda/CHANGELOG.md
    
    Co-authored-by: Jérémy Derussé <jeremy@derusse.com>

--- src/Service/Lambda/src/Enum/Runtime.php
+++ src/Service/Lambda/src/Enum/Runtime.php
@@ -20,6 +20,7 @@ final class Runtime
     public const NODEJS_10_X = 'nodejs10.x';
     public const NODEJS_12_X = 'nodejs12.x';
     public const NODEJS_14_X = 'nodejs14.x';
+    public const NODEJS_16_X = 'nodejs16.x';
     public const NODEJS_4_3 = 'nodejs4.3';
     public const NODEJS_4_3_EDGE = 'nodejs4.3-edge';
     public const NODEJS_6_10 = 'nodejs6.10';
@@ -50,6 +51,7 @@ final class Runtime
             self::NODEJS_10_X => true,
             self::NODEJS_12_X => true,
             self::NODEJS_14_X => true,
+            self::NODEJS_16_X => true,
             self::NODEJS_4_3 => true,
             self::NODEJS_4_3_EDGE => true,
             self::NODEJS_6_10 => true,
