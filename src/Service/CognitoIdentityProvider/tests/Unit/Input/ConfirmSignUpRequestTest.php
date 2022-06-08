9a65c8bde382ee3a974a02e741b96959129d7aca
Neal Brooks <neali82@gmail.com>
2022-06-08 19:18:55 +0100

    remove commented-out example in test

--- src/Service/CognitoIdentityProvider/tests/Unit/Input/ConfirmSignUpRequestTest.php
+++ src/Service/CognitoIdentityProvider/tests/Unit/Input/ConfirmSignUpRequestTest.php
@@ -51,27 +51,4 @@ class ConfirmSignUpRequestTest extends TestCase
 
         self::assertRequestEqualsHttpRequest($expected, $input->request());
     }
-
-
-//$input = new ChangePasswordRequest([
-//'PreviousPassword' => '/0zvOPBZhSRt:CC48wp%Jv"MMo47xM',
-//'ProposedPassword' => 'p9jtshQDSgo_IOpk"wGuT?m%yG&4;R',
-//'AccessToken' => 'F7358A38BFF27816416D7846FC1EE',
-//]);
-//
-//    // see https://docs.aws.amazon.com/cognito-user-identity-pools/latest/APIReference/API_ChangePassword.html
-//$expected = '
-//                POST / HTTP/1.0
-//                Content-Type: application/x-amz-json-1.1
-//                X-AMZ-Target: AWSCognitoIdentityProviderService.ChangePassword
-//
-//                {
-//                    "PreviousPassword": "/0zvOPBZhSRt:CC48wp%Jv\"MMo47xM",
-//                    "ProposedPassword": "p9jtshQDSgo_IOpk\"wGuT?m%yG&4;R",
-//                    "AccessToken": "F7358A38BFF27816416D7846FC1EE"
-//                }
-//                ';
-//
-//self::assertRequestEqualsHttpRequest($expected, $input->request());
-//}
 }
