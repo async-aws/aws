2e222f01c33a8a061f1d83508f6b26dc3bf3a400
Jiří Barouš <Amunak@users.noreply.github.com>
2023-01-12 11:01:10 +0100

    Fix test response
    Co-authored-by: Jérémy Derussé <jeremy@derusse.com>

--- src/Service/CloudWatchLogs/tests/Unit/Input/CreateLogStreamRequestTest.php
+++ src/Service/CloudWatchLogs/tests/Unit/Input/CreateLogStreamRequestTest.php
@@ -19,6 +19,7 @@ class CreateLogStreamRequestTest extends TestCase
 POST / HTTP/1.0
 Content-Type: application/x-amz-json-1.1
 X-Amz-Target: Logs_20140328.CreateLogStream
+
 {
   "logGroupName": "my-log-group",
   "logStreamName": "my-log-stream"
