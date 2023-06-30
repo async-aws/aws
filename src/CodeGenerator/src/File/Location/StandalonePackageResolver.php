3caaf5875246c7409f4bc6c3abc35aa8fae68725
Christophe Coevoet <stof@notk.org>
2023-06-30 14:20:22 +0200

    Implement a directory resolver for standalone packages
    This resolver supports the use case of generating a standalone package
    containing only one service, where the package is at the root of the
    repository.

---
+++ src/CodeGenerator/src/File/Location/StandalonePackageResolver.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace AsyncAws\CodeGenerator\File\Location;
+
+class StandalonePackageResolver implements DirectoryResolver
+{
+    /**
+     * @var string
+     */
+    private $rootDirectory;
+
+    /**
+     * @var string
+     */
+    private $rootNamespace;
+
+    public function __construct(string $rootDirectory, string $rootNamespace)
+    {
+        $this->rootDirectory = $rootDirectory;
+        $this->rootNamespace = $rootNamespace;
+    }
+
+    public function resolveDirectory(string $namespace): string
+    {
+        if ($namespace === $this->rootNamespace) {
+            return $this->rootDirectory . '/src';
+        }
+
+        if (0 !== strpos($namespace, $this->rootNamespace . '\\')) {
+            throw new \InvalidArgumentException('The namespace must belong to the generated package.');
+        }
+
+        $relativeNamespace = substr($namespace, \strlen($this->rootNamespace . '\\'));
+
+        $parts = explode('\\', $relativeNamespace);
+        if (isset($parts[0]) && 'Tests' === $parts[0]) {
+            array_shift($parts); // Tests
+            array_unshift($parts, 'tests');
+        } else {
+            array_unshift($parts, 'src');
+        }
+
+        return sprintf('%s/%s', $this->rootDirectory, implode('/', $parts));
+    }
+}
