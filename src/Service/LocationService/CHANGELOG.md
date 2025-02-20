ab9d78e95d228c53a7f656b5600cf6419d1a8276
AsyncAws <61784373+async-aws-bot@users.noreply.github.com>
2025-02-20 00:04:43 -0800

    Update generated code (#1846)
    * update generated code
    
    * Update src/Service/LocationService/CHANGELOG.md
    
    ---------
    
    Co-authored-by: Jérémy Derussé <jeremy@derusse.com>

--- src/Service/LocationService/CHANGELOG.md
+++ src/Service/LocationService/CHANGELOG.md
@@ -2,6 +2,10 @@
 
 ## NOT RELEASED
 
+### Added
+
+- AWS api-change: Added `UNKNOWN_FIELD` constant in `ValidationExceptionReason`
+
 ## 1.0.4
 
 ### Changed
--- src/Service/LocationService/src/Enum/ValidationExceptionReason.php
+++ src/Service/LocationService/src/Enum/ValidationExceptionReason.php
@@ -8,6 +8,7 @@ final class ValidationExceptionReason
     public const FIELD_VALIDATION_FAILED = 'FieldValidationFailed';
     public const MISSING = 'Missing';
     public const OTHER = 'Other';
+    public const UNKNOWN_FIELD = 'UnknownField';
     public const UNKNOWN_OPERATION = 'UnknownOperation';
 
     public static function exists(string $value): bool
@@ -17,6 +18,7 @@ final class ValidationExceptionReason
             self::FIELD_VALIDATION_FAILED => true,
             self::MISSING => true,
             self::OTHER => true,
+            self::UNKNOWN_FIELD => true,
             self::UNKNOWN_OPERATION => true,
         ][$value]);
     }
