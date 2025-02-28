ee4cd0c61dde685fbe22853863aaf8914bf7ba75
Victor Thuillier <victor@Users-MacBook-Pro.local>
2025-02-28 17:35:46 +0100

    Input and Result unit tests for BedrockRuntime InvokeModel operation

--- src/Service/BedrockRuntime/tests/Unit/Input/InvokeModelRequestTest.php
+++ src/Service/BedrockRuntime/tests/Unit/Input/InvokeModelRequestTest.php
@@ -9,28 +9,29 @@ class InvokeModelRequestTest extends TestCase
 {
     public function testRequest(): void
     {
-        self::fail('Not implemented');
-
         $input = new InvokeModelRequest([
-            'body' => 'change me',
-            'contentType' => 'change me',
-            'accept' => 'change me',
-            'modelId' => 'change me',
-            'trace' => 'change me',
-            'guardrailIdentifier' => 'change me',
-            'guardrailVersion' => 'change me',
-            'performanceConfigLatency' => 'change me',
+            'body' => '{"anthropic_version":"bedrock-2023-05-31","max_tokens":4096,"messages":[{"role":"user","content":[{"type":"text","text":"Write me a love poem."}]}],"temperature":1}',
+            'contentType' => 'application/json',
+            'accept' => 'application/json',
+            'modelId' => 'us.anthropic.claude-3-7-sonnet-20250219-v1:0',
+            'trace' => 'DISABLED',
+            'guardrailIdentifier' => 'arn:aws:bedrock:eu-west-1:965624758642:guardrail/azertyuiopqs',
+            'guardrailVersion' => 'DRAFT',
+            'performanceConfigLatency' => 'standard',
         ]);
 
         // see https://docs.aws.amazon.com/bedrock/latest/APIReference/API_runtime_InvokeModel.html
         $expected = '
-            POST / HTTP/1.0
+            POST /model/us.anthropic.claude-3-7-sonnet-20250219-v1:0/invoke HTTP/1.0
             Content-Type: application/json
+            Accept: application/json
+            X-Amzn-Bedrock-GuardrailIdentifier: arn:aws:bedrock:eu-west-1:965624758642:guardrail/azertyuiopqs
+            X-Amzn-Bedrock-GuardrailVersion: DRAFT
+            X-Amzn-Bedrock-PerformanceConfig-Latency: standard
+            X-Amzn-Bedrock-Trace: DISABLED
 
-            {
-            "change": "it"
-        }
-                ';
+            {"anthropic_version":"bedrock-2023-05-31","max_tokens":4096,"messages":[{"role":"user","content":[{"type":"text","text":"Write me a love poem."}]}],"temperature":1}
+        ';
 
         self::assertRequestEqualsHttpRequest($expected, $input->request());
     }
